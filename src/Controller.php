8cb70f4ebed0c753fb7ff3fc9d3d94a1935efcd9
xocode <overcode@amuz.co.kr>
2020-10-08 19:37:25 +0900

    기본 플러그인 생성

---
+++ src/Controller.php
@@ -0,0 +1,23 @@
+<?php
+namespace Overcode\XePlugin\DynamicFactory;
+
+use XeFrontend;
+use XePresenter;
+use App\Http\Controllers\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    public function index()
+    {
+        $title = 'DynamicFactory plugin';
+
+        // set browser title
+        XeFrontend::title($title);
+
+        // load css file
+        XeFrontend::css(Plugin::asset('assets/style.css'))->load();
+
+        // output
+        return XePresenter::make('dynamic_factory::views.index', ['title' => $title]);
+    }
+}
