10af4f5ba022aa37f4680ffb06de1559de9da440
xocode <overcode@amuz.co.kr>
2020-11-05 19:32:07 +0900

    CptDocument 모델 생성

---
+++ src/Models/CptDocument.php
@@ -0,0 +1,77 @@
+<?php
+namespace Overcode\XePlugin\DynamicFactory\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Xpressengine\Document\Models\Document;
+use Xpressengine\Seo\SeoUsable;
+use Xpressengine\User\Models\Guest;
+use Xpressengine\User\Models\UnknownUser;
+
+class CptDocument extends Document implements SeoUsable
+{
+    use SoftDeletes;
+
+    protected $canonical;
+
+    public function getTitle()
+    {
+        $title = str_replace('"', '\"', $this->getAttribute('title'));
+
+        return $title;
+    }
+
+    public function getDescription()
+    {
+        return str_replace(
+            ['"', "\n"],
+            ['\"', ''],
+            $this->getAttribute('pure_content')
+        );
+    }
+
+    public function getKeyword()
+    {
+        return [];
+    }
+
+    public function getUrl()
+    {
+        return $this->canonical;
+    }
+
+    public function getAuthor()
+    {
+        if ($this->user !== null) {
+            return $this->user;
+        } elseif ($this->isGuest() === true) {
+            return new Guest;
+        } else {
+            return new UnknownUser;
+        }
+    }
+
+
+    public function getImages()
+    {
+        $images = [];
+
+        return $images;
+    }
+
+    public function setCanonical($url)
+    {
+        $this->canonical = $url;
+
+        return $this;
+    }
+
+    public function isGuest()
+    {
+        return $this->getAttribute('user_type') === self::USER_TYPE_GUEST;
+    }
+
+    public function content()
+    {
+        return compile($this->instance_id, $this->content, $this->format === static::FORMAT_HTML);
+    }
+}
