a7c9036da7a5b53d39d9d921139b1feca1448597
xocode <overcode@amuz.co.kr>
2020-11-13 21:06:48 +0900

    카테고리에 확장필드 탭 추가

--- views/settings/taxonomy_extra.blade.php
+++ views/settings/taxonomy_extra.blade.php
@@ -1,6 +1,9 @@
+@section('page_title')
+    <h2>{{ xe_trans($category->name)}} - 확장필드</h2>
+@endsection
 <ul class="nav nav-tabs">
-    <li><a href="{{ route('dyFac.setting.create_taxonomy', ['tax_id' => 1]) }}">기본정보</a></li>
-    <li class="active"><a href="{{ route('dyFac.setting.taxonomy_extra', ['category_slug' => 1]) }}">확장필드</a></li>
+    <li><a href="{{ route('dyFac.setting.create_taxonomy', ['tax_id' => $cateExtra->category_id]) }}">기본정보</a></li>
+    <li class="active"><a href="{{ route('dyFac.setting.taxonomy_extra', ['category_slug' => $cateExtra->slug]) }}">확장필드</a></li>
 </ul>
 <div class="row">
     <div class="col-sm-12">
