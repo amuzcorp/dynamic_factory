e3362c72ea0e716eadba86d6b0e5a1555cb5d228
xocode <overcode@amuz.co.kr>
2020-12-30 11:48:17 +0900

    카테고리 템플릿 Single, Multi 만 남기고 삭제

--- views/settings/create_taxonomy.blade.php
+++ views/settings/create_taxonomy.blade.php
@@ -39,10 +39,8 @@
                             <label class="col-sm-2 col-form-label">템플릿</label>
                             <div class="col-sm-10">
                                 <select class="form-control" name="template">
-                                    <option value="select" @if($cpt_cate_extra->template === 'select') selected="selected" @endif>Select</option>
+                                    <option value="select" @if($cpt_cate_extra->template === 'select') selected="selected" @endif>Single Select</option>
                                     <option value="multi_select" @if($cpt_cate_extra->template === 'multi_select') selected="selected" @endif>Multi Select</option>
-{{--                                    <option value="hierarchy" @if($cpt_cate_extra->template === 'hierarchy') selected="selected" @endif>Hierarchy</option>--}}
-                                    <option value="check_list" @if($cpt_cate_extra->template === 'check_list') selected="selected" @endif>Check List</option>
                                 </select>
                             </div>
                         </div>
