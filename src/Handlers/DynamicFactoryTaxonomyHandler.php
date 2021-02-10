2b2c03765059c7e8f9e8b712b31e9f7f04b2096d
xocode <overcode@amuz.co.kr>
2021-02-10 16:20:23 +0900

    Resource 카테고리 하위사이트도 자동 등록

--- src/Handlers/DynamicFactoryTaxonomyHandler.php
+++ src/Handlers/DynamicFactoryTaxonomyHandler.php
@@ -95,30 +95,32 @@ class DynamicFactoryTaxonomyHandler
         \XeDB::beginTransaction();
         try {
             foreach((array)$df_categories as $cate) {
-                $slug = $cate['slug'];
-                $cate_extra = $this->getCategoryExtraBySlug($slug);
-                if (!isset($cate_extra)) {
-                    $langKey = XeLang::genUserKey();
-                    XeLang::save($langKey, 'ko', $cate['name'], false);
+                if(!isset($cate['share']) || $cate['share'] == false) {
+                    $slug = $cate['slug'];
+                    $cate_extra = $this->getCategoryExtraBySlug($slug);
+                    if (!isset($cate_extra)) {
+                        $langKey = XeLang::genUserKey();
+                        XeLang::save($langKey, 'ko', $cate['name'], false);
 
-                    $category = $this->categoryHandler->createCate(['name' => $langKey]);
-                    $category_id = $category->id;
+                        $category = $this->categoryHandler->createCate(['name' => $langKey]);
+                        $category_id = $category->id;
 
-                    $this->addCategoryItemForOut($category, $cate['items']);
+                        $this->addCategoryItemForOut($category, $cate['items']);
 
-                    $cateExtra = new CategoryExtra();
-                    $cateExtra->site_key = \XeSite::getCurrentSiteKey();
-                    $cateExtra->category_id = $category_id;
-                    $cateExtra->slug = $slug;
-                    $cateExtra->template = $cate['template'];
-                    $cateExtra->save();
+                        $cateExtra = new CategoryExtra();
+                        $cateExtra->site_key = \XeSite::getCurrentSiteKey();
+                        $cateExtra->category_id = $category_id;
+                        $cateExtra->slug = $slug;
+                        $cateExtra->template = $cate['template'];
+                        $cateExtra->save();
 
-                    foreach ($cate['cpt_ids'] as $cpt_id) {
-                        $cptTaxonomy = new CptTaxonomy();
-                        $cptTaxonomy->site_key = \XeSite::getCurrentSiteKey();
-                        $cptTaxonomy->cpt_id = $cpt_id;
-                        $cptTaxonomy->category_id = $category_id;
-                        $cptTaxonomy->save();
+                        foreach ($cate['cpt_ids'] as $cpt_id) {
+                            $cptTaxonomy = new CptTaxonomy();
+                            $cptTaxonomy->site_key = \XeSite::getCurrentSiteKey();
+                            $cptTaxonomy->cpt_id = $cpt_id;
+                            $cptTaxonomy->category_id = $category_id;
+                            $cptTaxonomy->save();
+                        }
                     }
                 }
             }
@@ -342,7 +344,8 @@ class DynamicFactoryTaxonomyHandler
 
     public function getCategoryExtraBySlug($slug)
     {
-        return CategoryExtra::where('slug', $slug)->first();
+        $siteKey = \XeSite::getCurrentSiteKey();
+        return CategoryExtra::where('slug', $slug)->where('site_key', $siteKey)->first();
     }
 
     public function getCategoryExtras()
