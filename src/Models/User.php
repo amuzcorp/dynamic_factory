3ba74f797514f0fdd2d7f8ea804e0169ea3d55aa
xocode <overcode@amuz.co.kr>
2021-05-03 17:27:00 +0900

    관련문서 처리

--- src/Models/User.php
+++ src/Models/User.php
@@ -1,12 +1,23 @@
 <?php
 namespace Overcode\XePlugin\DynamicFactory\Models;
 
+use Overcode\XePlugin\DynamicFactory\Components\DynamicFields\SuperRelate\SuperRelateField;
 use Xpressengine\User\Models\User as XeUser;
 
 class User  extends XeUser {
 
-    public function relateUser()
+    public function hasDocument($field_id, $use_dynamic = true)
     {
+        $tableName = SuperRelateField::TABLE_NAME;
+
+        $query = $this->belongsToMany(CptDocument::class, $tableName, 's_id', 't_id')->where($tableName.'.field_id', $field_id);
+        if($use_dynamic){
+            $target_group = SuperRelate::Where('field_id', $field_id)->where('s_id', $this->id)->where('s_group', 'user')->pluck('t_group')->first();
+
+            $query->setProxyOption(['group' => $target_group, 'table' => 'documents'], false);
+        }
+
+        return $query->get();
     }
 
 }
