a2c4289cf972856a7e5adf474628321af31e2aa3
zoozkwon <!qkqhfksek7603>
2021-07-05 17:00:58 +0900

    edit용 스타일, 스크립트 추가

--- views/settings/create_extra.blade.php
+++ views/settings/create_extra.blade.php
@@ -1,6 +1,12 @@
 @section('page_title')
     <h2>{{ $cpt['cpt_name'] }} - 확장 필드</h2>
 @endsection
+<!-- CSS -->
+{{XeFrontend::css('assets/vendor/bootstrap/css/bootstrap.min.css')->load()}}
+{{XeFrontend::css('assets/core/settings/css/admin.css')->load()}}
+{{XeFrontend::css('plugins/ckeditor/assets/css/editor.css')->load()}}
+{{XeFrontend::css('plugins/ckeditor/assets/css/content.css')->load()}}
+
 
 @include('dynamic_factory::views.settings.tab')
 <div class="row">
@@ -16,3 +22,16 @@
         </div>
     </div>
 </div>
+
+<!-- 에디터 스크립트 -->
+{{ XeFrontend::js([
+    'assets/vendor/jqueryui/jquery-ui.min.js',
+    'assets/core/editor/editor.bundle.js',
+    'assets/vendor/jQuery-File-Upload/js/vendor/jquery.ui.widget.js',
+    'assets/vendor/jQuery-File-Upload/js/jquery.iframe-transport.js',
+    'assets/vendor/jQuery-File-Upload/js/jquery.fileupload.js',
+    'plugins/ckeditor/assets/ckeditor/ckeditor.js',
+    'plugins/ckeditor/assets/ckeditor/styles.js',
+    'plugins/ckeditor/assets/js/media_library.widget.js',
+    'plugins/ckeditor/assets/js/xe.ckeditor.define.js'
+])->load() }}
