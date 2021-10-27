78d6bf1662256c29519b860c7dbf477deeb9e566
zoozkwon <!qkqhfksek7603>
2021-10-27 18:24:52 +0900

    오류 테스트

--- src/Services/DynamicFactoryService.php
+++ src/Services/DynamicFactoryService.php
@@ -311,7 +311,6 @@ class DynamicFactoryService
         if($site_key != '*' && Schema::hasColumn('documents', 'site_key')){
             $site_key = $site_key != null ? $site_key : \XeSite::getCurrentSitekey();
             $query->where('site_key', $site_key);
-            dd($site_key, $query->get());
         }
 
 //        $query->visible(); // trash 가 아닌것만
@@ -321,6 +320,7 @@ class DynamicFactoryService
                 $query = $handler->getItems($query, $attributes);
             }
         }
+        dd($site_key, $query->get());
 
         return $query;
     }
