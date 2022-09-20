e3235fe89cc24ad8c22a13d27078428e976f25cb
zoozkwon <zooz0806@gmail.com>
2022-09-20 15:34:05 +0900

    쿼리문 조회 orderBy 테스트

--- src/Controllers/DynamicFactorySettingController.php
+++ src/Controllers/DynamicFactorySettingController.php
@@ -605,8 +605,9 @@ class DynamicFactorySettingController extends BaseController
         $perPage = (int) $request->get('perPage', '10');
 
         if($request->get('test', 0)  == 22) {
-            $testIds = \XeDB::table('documents')->where('instance_id', $cpt->cpt_id)->pluck('id');
-
+            $testIds = \XeDB::table('documents')->where('instance_id', $cpt->cpt_id);
+            $test = $this->makeWhere($testIds, $request);
+            dd($test);
             $query = $this->dfService->getItemsWhereQuery(array_merge($request->all(), [
                 'force' => true,
                 'cpt_id' => $cpt->cpt_id
