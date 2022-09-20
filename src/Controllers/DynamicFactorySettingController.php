98e150f3b745956b66fb575086180c0396dfac1a
zoozkwon <zooz0806@gmail.com>
2022-09-20 12:49:03 +0900

    쿼리문 조회 orderBy 테스트

--- src/Controllers/DynamicFactorySettingController.php
+++ src/Controllers/DynamicFactorySettingController.php
@@ -1047,25 +1047,25 @@ class DynamicFactorySettingController extends BaseController
 
         //TODO orderBy 오류 있어서 임시 제거
         //TODO 부산경총 오류
-        if ($orderType == '' && $request->get('test', 0)  != 88) {
-            // order_type 이 없을때만 dyFac Config 의 정렬을 우선 적용한다.
-            $orders = $config->get('orders', []);
-            foreach ($orders as $order) {
-                $arr_order = explode('|@|',$order);
-                $sort = 'asc';
-                if($arr_order[1] === 'asc') $sort = 'desc';
-                $query->orderBy($arr_order[0], $sort);
-            }
-            $query->orderBy('head', 'asc');
-        } elseif ($orderType == 'assent_count') {
-            $query->orderBy('assent_count', 'asc')->orderBy('head', 'asc');
-        } elseif ($orderType == 'recently_created') {
-            $query->orderBy(CptDocument::CREATED_AT, 'asc')->orderBy('head', 'asc');
-        } elseif ($orderType == 'recently_published') {
-            $query->orderBy('published_at', 'asc')->orderBy('head', 'asc');
-        } elseif ($orderType == 'recently_updated') {
-            $query->orderBy(CptDocument::UPDATED_AT, 'asc')->orderBy('head', 'asc');
-        }
+//        if ($orderType == '' && $request->get('test', 0)  != 88) {
+//            // order_type 이 없을때만 dyFac Config 의 정렬을 우선 적용한다.
+//            $orders = $config->get('orders', []);
+//            foreach ($orders as $order) {
+//                $arr_order = explode('|@|',$order);
+//                $sort = 'asc';
+//                if($arr_order[1] === 'asc') $sort = 'desc';
+//                $query->orderBy($arr_order[0], $sort);
+//            }
+//            $query->orderBy('head', 'asc');
+//        } elseif ($orderType == 'assent_count') {
+//            $query->orderBy('assent_count', 'asc')->orderBy('head', 'asc');
+//        } elseif ($orderType == 'recently_created') {
+//            $query->orderBy(CptDocument::CREATED_AT, 'asc')->orderBy('head', 'asc');
+//        } elseif ($orderType == 'recently_published') {
+//            $query->orderBy('published_at', 'asc')->orderBy('head', 'asc');
+//        } elseif ($orderType == 'recently_updated') {
+//            $query->orderBy(CptDocument::UPDATED_AT, 'asc')->orderBy('head', 'asc');
+//        }
 
         return $query->paginate(20, ['*'], 'page', $page);
     }
