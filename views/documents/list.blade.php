13d5be0ed82f5792a51e8d2919f93c04e90e1725
zoozkwon <zooz0806@gmail.com>
2022-10-26 12:56:49 +0900

    문서 Excel Download 추가

--- views/documents/list.blade.php
+++ views/documents/list.blade.php
@@ -63,7 +63,7 @@ foreach($data as $id => $value){
                                 <input type="file" class="__xe_file xe-hidden" name="csv_file" accept=".csv" onchange="uploadCSV(this)">
                             </label>
                             <a onclick="downloadCSV()" class="xe-btn xe-btn-success-outline"><i class="xi-download"></i>CSV 저장</a>
-                            <a onclick="downloadExcel()" class="xe-btn xe-btn-success-outline"><i class="xi-download"></i>엑셀 저장</a>
+                            <a onclick="downloadExcel()" class="xe-btn xe-btn-primary-outline"><i class="xi-download"></i>엑셀 저장</a>
                             <a href="{{ route('dyFac.setting.edit', ['cpt_id' => $cpt->cpt_id]) }}" class="xe-btn xe-btn-positive-outline"><i class="xi-cog"></i> 설정</a>
                             <a href="{{ route($current_route_name, ['type' => 'create']) }}" class="xe-btn xe-btn-primary" data-toggle="xe-page-modal"><i class="xi-file-text-o"></i> {{ sprintf($cpt->labels['new_add_cpt'], $cpt->cpt_name) }}</a>
                         </div>
