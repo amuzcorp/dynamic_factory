889e7dd6395bc47b6ea0c6f5cca2332eca66da1d
zoozkwon <zooz0806@gmail.com>
2023-11-28 13:01:42 +0900

    카테고리 임의 생성, 삭제 불가능하도록 수정 아뮤즈 계정만 가능

--- views/settings/category.blade.php
+++ views/settings/category.blade.php
@@ -12,7 +12,9 @@
                 <div class="panel-heading">
                     <div class="pull-left"><h4>{{xe_trans('dyFac::gen_category_list')}}</h4></div>
                     <div class="pull-right">
-                        <a href="{{ route('dyFac.setting.create_taxonomy') }}" class="xe-btn xe-btn-primary">{{xe_trans('dyFac::create_new_category')}}</a>
+                        @if(\Auth::user()->login_id == 'amuzcorp')
+                            <a href="{{ route('dyFac.setting.create_taxonomy') }}" class="xe-btn xe-btn-primary">{{xe_trans('dyFac::create_new_category')}}</a>
+                        @endif
                     </div>
                 </div>
                 <div class="panel-body">
@@ -35,19 +37,21 @@
                                 <td>{{ xe_trans($category->category->name) }}</td>
                                 <td>{{ $category->template }}</td>
                                 <td>
-                                @foreach($category->cpt_tax as $cpt_tax)
-                                    @if(!empty(app('overcode.df.service')->getItem($cpt_tax->cpt_id)->cpt_name))
-                                    <a href="{{ route('dyFac.setting.edit', ['cpt_id' => $cpt_tax->cpt_id]) }}">
+                                    @foreach($category->cpt_tax as $cpt_tax)
+                                        @if(!empty(app('overcode.df.service')->getItem($cpt_tax->cpt_id)->cpt_name))
+                                            <a href="{{ route('dyFac.setting.edit', ['cpt_id' => $cpt_tax->cpt_id]) }}">
                                         <span class="label label-info">
                                         {{ app('overcode.df.service')->getItem($cpt_tax->cpt_id)->cpt_name }}
                                         </span>
-                                    </a>
-                                    @endif
-                                @endforeach
+                                            </a>
+                                        @endif
+                                    @endforeach
                                 </td>
                                 <td>
                                     <a class="xe-btn xe-btn-default xe-btn-xs" href="{{ route('dyFac.setting.create_taxonomy',[ 'tax_id' => $category->category_id]) }}">설정</a>
-                                    <a class="xe-btn xe-btn-danger xe-btn-xs cate_del_btn" style="color:#fff;" data-category_id="{{ $category->category_id }}">삭제</a>
+                                    @if(\Auth::user()->login_id == 'amuzcorp')
+                                        <a class="xe-btn xe-btn-danger xe-btn-xs cate_del_btn" style="color:#fff;" data-category_id="{{ $category->category_id }}">삭제</a>
+                                    @endif
                                 </td>
                             </tr>
                         @endforeach
@@ -59,21 +63,21 @@
     </div>
 </div>
 <script type="text/javascript">
-$(document).ready(function() {
-    $('.cate_del_btn').on('click', function() {
-        var category_id = $(this).data('category_id');
+    $(document).ready(function() {
+        $('.cate_del_btn').on('click', function() {
+            var category_id = $(this).data('category_id');
 
-        if(confirm('정말 이 카테고리를 삭제 하시겠습니까?')) {
-            window.XE.ajax({
-                url: '{{ route('dyFac.setting.category.delete') }}',
-                type: 'post',
-                dataType: 'json',
-                data: {id: category_id}
-            }).done(function (json) {
-                console.log(json);
-                location.reload();
-            })
-        }
+            if(confirm('정말 이 카테고리를 삭제 하시겠습니까?')) {
+                window.XE.ajax({
+                    url: '{{ route('dyFac.setting.category.delete') }}',
+                    type: 'post',
+                    dataType: 'json',
+                    data: {id: category_id}
+                }).done(function (json) {
+                    console.log(json);
+                    location.reload();
+                })
+            }
+        });
     });
-});
 </script>
